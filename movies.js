25540b80b7179fb9bd8262f605f8bf201ac6792c
Felecia Davis <felecia.m.davis1@gmail.com>
2021-04-20 16:52:54 -0400

    feat: added movies html and js files.

--- movies.js
+++ movies.js
@@ -3,7 +3,32 @@
 
 $(document).ready(function(){
 
+    $( "#submit-movie" ).click(function() {
+        let movieTitle = $("#movie-title").val();
+        let movieRating = $("#movie-rating").val();
+        let addMovie = {
+            title : movieTitle,
+            rating : movieRating
+        }
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(addMovie),
+        };
+        fetch("https://rambunctious-fabulous-gopher.glitch.me/movies", options)
+            .then( response => console.log(response) ) /* review was created successfully */
+            .catch( error => console.error(error) ); /* handle errors */
+        fetch("https://rambunctious-fabulous-gopher.glitch.me/movies")
+            .then(response => (response.json())) // parse the JSON from the server
+            .then(response => {
+                appendNewMovie(response)})
 
+
+
+
+    });
 
 const moviesList = document.getElementById("movies");
 const moviesForm = document.querySelector("form");
@@ -15,13 +40,29 @@ function appendNewMovie(movies) {
     let newListItem = "";
 
     for(let i = 0; i < movies.length; i++){
-        newListItem += `<li>${movies[i].title}</li>`;
-        console.log(movies[i].title);
+        //movies.forEach(function(movie) {
+
+
+            newListItem += `<li>${movies[i].title} <span>${movies[i].rating}</span> </li>`;
+            console.log(movies[i].title);
+        //})
     }
-    console.log(newListItem);
+     console.log(newListItem);
     moviesList.innerHTML = newListItem;
 
 }
+
+
+
+    // $( "#submit-movie" ).click(function() {
+    //     let movieTitle = $("#movie-title").html();
+    //     let movieRating = $("#movie-rating").html();
+    //     console.log(movieTitle);
+    //     console.log(movieRating);
+    //
+    // });
+
+
 
 fetch("https://rambunctious-fabulous-gopher.glitch.me/movies")
     .then(response => (response.json())) // parse the JSON from the server
@@ -37,17 +78,33 @@ fetch("https://rambunctious-fabulous-gopher.glitch.me/movies")
             }
             console.log(moviesObj);
 
-            const options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(moviesObj),
-            };
-            fetch(moviesApiURL, options).then(function (response){
-                console.log(response);
-            }).then(response => fetch("/movies").then(response => (response.json())).then(response => {appendNewMovie(response);}))
+            // const options = {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify(moviesObj),
+            // };
+            //
+            // fetch(moviesApiURL, options).then(function (response){
+            //     console.log(response);
+            // }).then(response => fetch("/movies").then(response => (response.json())).then(response => {appendNewMovie(response);}))
+            //
 
+            // let putURL = "https://rambunctious-fabulous-gopher.glitch.me/movies"
+            //
+            // let newDreamObj = {"dream": "My dream is to be a successful Software Developer"}
+            // let options = {
+            //     method: "PUT",
+            //     headers: {
+            //         "Content-Type": "application/json",
+            //     },
+            //     body: JSON.stringify(newDreamObj)
+            // }
+            //
+            // fetch(putURL, options).then(function(response){
+            //     console.log(response)
+            // })
         });
     });
 
